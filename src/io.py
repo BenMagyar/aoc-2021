66de8fafbcd2946a224ece91f55529a3b1354fa6
Ben Magyar <bmagyar@etsy.com>
2021-12-02 15:07:27 -0500

    Adding start script

--- src/io.py
+++ src/io.py
@@ -1,9 +1,12 @@
 import os
 
+def clean(lines):
+    return [line for line in lines if not line.isspace()]
+
 def read_example(day):
     with open(os.path.join(os.getcwd(), "examples", day + ".txt")) as file:
-        return file.readlines()
+        return clean(file.readlines())
 
 def read_input(day):
     with open(os.path.join(os.getcwd(), "inputs", day + ".txt")) as file:
-        return file.readlines()
+        return clean(file.readlines())
